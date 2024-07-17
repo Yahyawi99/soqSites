a5da72d78dcc2a50e058def4021f4f9090560b3f
Yahyawi99 <yassinyahyawi26@gmail.com>
2024-07-17 15:41:15 +0100

    working on intro

--- components/home/Intro.js
+++ components/home/Intro.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 // components
 import Header from "../shared/Header";
 // styles
@@ -13,6 +14,33 @@ function Intro() {
         <h1>
           Transform your <span>online</span> presence
         </h1>
+
+        <p>
+          <span>Based in Casablanca</span>, Morocco, Soqsites is your go-to web
+          digital agency for launching digital products swiftly and effectively.
+          Beyond code compliance, we advocate for your business from every
+          angle, ensuring full support and market success.
+        </p>
+      </div>
+
+      <div className={styles.bgPattern1}>
+        <Image
+          src="/assets/bg-pattern-home-1.svg"
+          width={100}
+          height={100}
+          alt="bg-pattern-1"
+          layout="responsive"
+        />
+      </div>
+
+      <div className={styles.bgPattern2}>
+        <Image
+          src="/assets/bg-pattern-home-2.svg"
+          width={100}
+          height={100}
+          alt="bg-pattern-1"
+          layout="responsive"
+        />
       </div>
     </section>
   );
