6eefbb33958dee49fd74ae96ad3bd2b149d230ea
Yahyawi99 <yassinyahyawi26@gmail.com>
2024-07-23 16:18:25 +0100

    working on services home section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Intro from "../components/home/Intro";
 import Features from "../components/home/features";
+import Services from "../components/home/services";
 
 export default function Home() {
   return (
     <main>
       <Intro />
       <Features />
+      <Services />
     </main>
   );
 }
--- components/home/services.js
+++ components/home/services.js
@@ -4,7 +4,54 @@ import Image from "next/image";
 import styles from "../../styles/home/services.module.css";
 
 function index() {
-  return <section className={styles.services}></section>;
+  return (
+    <section className={styles.services}>
+      <div>
+        <h2>Our Services</h2>
+
+        <div>
+          <div className={styles.service}>
+            <div>
+              <Image
+                src="/assets/webDev-logo.jpg"
+                alt="contract"
+                width={50}
+                height={50}
+                layout="responsive"
+              />
+            </div>
+            <button>WEB DEVELOPMENT</button>
+          </div>
+
+          <div className={styles.service}>
+            <div>
+              <Image
+                src="/assets/design-logo.svg"
+                alt="contract"
+                width={50}
+                height={50}
+                layout="responsive"
+              />
+            </div>
+            <button>LOGO DESIGN</button>
+          </div>
+
+          <div className={styles.service}>
+            <div>
+              <Image
+                src="/assets/seo-logo.svg"
+                alt="contract"
+                width={50}
+                height={50}
+                layout="responsive"
+              />
+            </div>
+            <button>SEO SERVICES</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default index;
